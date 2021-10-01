f9ad478c2d79bc7f3217d2c5b14da9cb18088ccb
shahidmonowarr <shahidmonowar21@gmail.com>
2021-10-01 11:33:54 +0600

    added

---
+++ src/components/Inventory/Inventory.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Inventory = () => {
+    return (
+        <div>
+            <h2>this is Inventory</h2>
+        </div>
+    );
+};
+
+export default Inventory;
